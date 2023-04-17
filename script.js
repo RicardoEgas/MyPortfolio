053c95a75e6e4b7c6a86805b50490c2f58250416
RicardoEgas <rj_egas@hotmail.com>
2023-04-17 17:56:14 +0200

    Built mobile menu

---
+++ script.js
@@ -0,0 +1,19 @@
+const menu = document.querySelector('.Menu');
+const burger = document.querySelector('.burger');
+const links = document.querySelectorAll('.portfolio-links');
+
+
+
+function toggleMenu() {
+
+  menu.classList.toggle('active');
+
+}
+
+function close() {
+    menu.classList.remove('active');
+}
+
+
+burger.addEventListener('click', toggleMenu);
+links.forEach(n => n.addEventListener('click', close));
