5c7cfe7a04a404ba65caf2fb55e64d5e4de53b38
RicardoEgas <rj_egas@hotmail.com>
2023-04-18 10:32:20 +0200

    Fixed Linter errors

--- script.js
+++ script.js
@@ -2,18 +2,13 @@ const menu = document.querySelector('.Menu');
 const burger = document.querySelector('.burger');
 const links = document.querySelectorAll('.portfolio-links');
 
-
-
 function toggleMenu() {
-
   menu.classList.toggle('active');
-
 }
 
 function close() {
-    menu.classList.remove('active');
+  menu.classList.remove('active');
 }
 
-
 burger.addEventListener('click', toggleMenu);
-links.forEach(n => n.addEventListener('click', close));
+links.forEach((n) => n.addEventListener('click', close));
