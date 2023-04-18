2b97e5ed291c14d0195cb9461928a3d76fa867eb
RicardoEgas <rj_egas@hotmail.com>
2023-04-18 15:48:06 +0200

    Fixed padding and added pictures to menu

--- script.js
+++ script.js
@@ -1,13 +1,16 @@
 const menu = document.querySelector('.Menu');
 const burger = document.querySelector('.burger');
 const links = document.querySelectorAll('.portfolio-links');
+const header = document.querySelector('header');
 
 function toggleMenu() {
   menu.classList.toggle('active');
+  header.classList.toggle('active');
 }
 
 function close() {
   menu.classList.remove('active');
+  header.classList.remove('active');
 }
 
 burger.addEventListener('click', toggleMenu);
