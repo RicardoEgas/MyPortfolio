74dc81d627ed2f59daea33592707a3bb036fc09f
RicardoEgas <rj_egas@hotmail.com>
2024-01-26 11:28:46 +0100

    Fix event listeners for see project buttons

--- script.js
+++ script.js
@@ -23,7 +23,7 @@ const details = [
     id: 1,
     name: 'Project name gors here',
     description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi. Ut aliquip ex ea commodo consequat.  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi.',
-    image: 'images/Group 109.png',
+    image: 'images/budget-app-screenshot.png',
     technologies: ['Html/CSS', 'Ruby on Rails', 'Javascript'],
     liveVersion: '#',
     source: '#',
@@ -32,7 +32,7 @@ const details = [
     id: 2,
     name: 'Project name gors here',
     description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi. Ut aliquip ex ea commodo consequat.  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi.',
-    image: 'images/Group 109.png',
+    image: 'images/BballGeeks-screenshot.png',
     technologies: ['Html/CSS', 'Ruby on Rails', 'Javascript'],
     liveVersion: '#',
     source: '#',
@@ -41,7 +41,7 @@ const details = [
     id: 3,
     name: 'Project name gors here',
     description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi. Ut aliquip ex ea commodo consequat.  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi.',
-    image: 'images/Group 109.png',
+    image: 'images/music-festival-website.png',
     technologies: ['Html/CSS', 'Ruby on Rails', 'Javascript'],
     liveVersion: '#',
     source: '#',
@@ -50,7 +50,7 @@ const details = [
     id: 4,
     name: 'Project name gors here',
     description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi. Ut aliquip ex ea commodo consequat.  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi.',
-    image: 'images/Group 109.png',
+    image: 'images/Rick&Morty-screenshot.png',
     technologies: ['Html/CSS', 'Ruby on Rails', 'Javascript'],
     liveVersion: '#',
     source: '#',
@@ -59,16 +59,7 @@ const details = [
     id: 5,
     name: 'Project name gors here',
     description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi. Ut aliquip ex ea commodo consequat.  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi.',
-    image: 'images/Group 109.png',
-    technologies: ['Html/CSS', 'Ruby on Rails', 'Javascript'],
-    liveVersion: '#',
-    source: '#',
-  },
-  {
-    id: 6,
-    name: 'Project name gors here',
-    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi. Ut aliquip ex ea commodo consequat.  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi.',
-    image: 'images/Group 109.png',
+    image: 'images/space-travelers-screenshot.png',
     technologies: ['Html/CSS', 'Ruby on Rails', 'Javascript'],
     liveVersion: '#',
     source: '#',
@@ -76,43 +67,43 @@ const details = [
 ];
 
 const projects = document.getElementById('allProjects');
+
 const images = details.map((detail) => detail.image);
-
 const names = details.map((detail) => detail.name);
-
 const technologies = details.map((detail) => detail.technologies);
-
 const description = details.map((detail) => detail.description);
-
-let z = 0;
-
-function openModal() {
-  projects.style.display = 'block';
-  projects.insertAdjacentHTML('beforeend', `<div id="modal">
-            <div class="cancel"><img src="images/ic_cross.jpg" alt=""></div>   
-            <h2 class="title-modal">${names[z]}</h2>
-            <ul class="tech-flex">
-                <li class="Html-css">${technologies[z][0]}</li>
-                <li class="Ruby">${technologies[z][1]}</li>
-                <li class="Javascript">${technologies[z][2]}</li>
-            </ul>
-            <div class="modal-image"><img src="${images[z]}" alt="project-image-desktop"></div>
-            <div class="descript">${description[z]}</div>
-            <button class="see-live">
-                See Live <i class="fa-sharp fa-light fa-arrow-up-right-from-square"></i>
-            </button>
-            <button class="see-source">
-            See Source<i class="fa-brands fa-github"></i>
-            </button>
-</div>`);
-  if (z > details.length) {
-    z += 1;
-  }
-}
 
 const openModalButtons = document.querySelectorAll('[data-modal-target]');
 
-openModalButtons.forEach((button) => button.addEventListener('click', openModal));
+openModalButtons.forEach((button) => {
+  button.addEventListener('click', (e) => {
+    const parentContainer = e.target.closest('.big-laptop, .laptop1, .laptop2, .laptop3, .laptop4, .laptop5, .grid3');
+
+    if (parentContainer) {
+      const image = parentContainer.querySelector('img');
+      const imageSrc = image.getAttribute('src'); 
+      const detail = details.find((detail) => detail.image === imageSrc);
+
+      if (detail) {
+        projects.style.display = 'block';
+        projects.innerHTML = `
+          <div id="modal">
+            <div class="cancel"><img src="images/ic_cross.jpg" alt=""></div>   
+            <h2 class="title-modal">${detail.name}</h2>
+            <ul class="tech-flex">
+              <li class="Html-css">${detail.technologies[0]}</li>
+              <li class="Ruby">${detail.technologies[1]}</li>
+              <li class="Javascript">${detail.technologies[2]}</li>
+            </ul>
+            <div class="modal-image"><img src="${detail.image}" alt="project-image-desktop"></div>
+            <div class="descript">${detail.description}</div>
+            <button class="see-live">See Live <i class="fa-sharp fa-light fa-arrow-up-right-from-square"></i></button>
+            <button class="see-source">See Source<i class="fa-brands fa-github"></i></button>
+          </div>`;
+      }
+    }
+  });
+});
 
 document.addEventListener('click', (e) => {
   const target = e.target.closest('.cancel');
